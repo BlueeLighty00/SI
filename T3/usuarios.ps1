f1995204c0e672298faec7bd6d1455d941625fe1
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2022-11-30 13:03:15 +0100

    Create usuarios.ps1

---
+++ T3/usuarios.ps1
@@ -0,0 +1,11 @@
+#Creación de usuarios de forma masiva.
+#Ayuda: https://learn.microsoft.com/es-es/powershell/module/microsoft.powershell.localaccounts/new-localuser?view=powershell-5.1
+
+$file_users=Import-Csv -Path usuarios.csv 
+foreach ($user in $file_users) { 
+  $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
+  New-LocalUser $user.cuenta -Password $clave -FullName $user.Fullname -Description $user.descripcion -AccountNeverExpires -PasswordNeverExpires
+  #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
+  Add-LocalGroupMember -Group $user.grupo -Member $user.cuenta
+  Add-LocalGroupMember -Group usuarios -Member $user.cuenta
+}
