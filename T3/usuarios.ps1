bad656eaebd77a77d56109b797546887e5a9c4e9
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2022-11-30 19:02:03 +0100

    Update usuarios.ps1

--- T3/usuarios.ps1
+++ T3/usuarios.ps1
@@ -5,6 +5,9 @@ $file_users=Import-Csv -Path usuarios.csv
 foreach ($user in $file_users) { 
   $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
   New-LocalUser $user.cuenta -Password $clave -FullName $user.Fullname -Description $user.descripcion -AccountNeverExpires -PasswordNeverExpires
+  #Indicamos que la primera vez que logueamos nos pida un cambio de contraseña
+  Set-LocalUser $user.cuenta -PasswordNeverExpires $false
+  net user $user.cuenta /logonpasswordchg:yes
   #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
   Add-LocalGroupMember -Group $user.grupo -Member $user.cuenta
   Add-LocalGroupMember -Group usuarios -Member $user.cuenta
