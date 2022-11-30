81c8f8628e979ca81b1ddf49941f6f07bebb5cc5
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2022-11-30 17:36:07 +0100

    Update delusugr.ps1

--- T3/delusugr.ps1
+++ T3/delusugr.ps1
@@ -1,6 +1,12 @@
 #Eliminación de usuarios
 
-$file_users=Import-Csv -Path usuarios.csv 
+$file_users=Import-Csv -Path C:\Users\dserna\Desktop\usuarios.csv 
 foreach ($user in $file_users) {
     Remove-LocalUser $user.cuenta
+}
+
+$file_groups=Import-Csv -Path C:\Users\dserna\Desktop\grupos.csv
+
+foreach ($group in $file_groups) { 
+  Remove-LocalGroup $group.nombre
 }
