4a87b3acfe4d8431f3f35bf07045d6b8e3cbf5c7
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-01-27 12:46:14 +0100

    Update AddGroupsUsers.sh

--- T4/AddGroupsUsers.sh
+++ T4/AddGroupsUsers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#Creación de grupos
 groupadd sinformacion
 groupadd desarrollo
 groupadd explotacion
@@ -15,9 +14,11 @@ groupadd src_w
 groupadd src_x
 
 
-#Creación de usuarios
-useradd -m -d /home/usu_sinformacion -G sinformacion -p sinformacion -s /bin/bash usu_sinformacion
-useradd -m -d /home/usu_desarrollo -G desarrollo -p desarrollo -s /bin/bash usu_desarrollo
-useradd -m -d /home/usu_explotacion -G explotacion -p explotacion -s /bin/bash usu_explotacion
 
+useradd -m -d /home/usu_sinformacion -G sinformacion -s /bin/bash usu_sinformacion
+useradd -m -d /home/usu_desarrollo -G desarrollo -s /bin/bash usu_desarrollo
+useradd -m -d /home/usu_explotacion -G explotacion -s /bin/bash usu_explotacion
 
+passwd usu_sinformacion
+passwd usu_desarrollo
+passwd usu_explotacion
