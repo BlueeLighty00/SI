72d8b83c20b697ecb9340e97e5db0d47a57c3d0f
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-01-29 15:36:17 +0100

    Update scriptAmpliacion.sh

--- T4/scriptAmpliacion.sh
+++ T4/scriptAmpliacion.sh
@@ -4,6 +4,6 @@ groupadd dinterfaces
 
 useradd -m -d /home/usu_dinterfaces -G dinterfaces -s /bin/bash usu_dinterfaces
 
-usermod -a -G proyectos_x,src_r,pruebas_r  usu_dinterfaces
+usermod -a -G proyectos_x,src_r,src_x,pruebas_r,pruebas_x usu_dinterfaces
 
 passwd usu_dinterfaces
