6452070453cd5951498c7435ba2da8d59d8436b3
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-03-28 19:50:30 +0200

    Update webcamPenyiscola.sh

--- T5/webcamPenyiscola.sh
+++ T5/webcamPenyiscola.sh
@@ -4,7 +4,7 @@ DATE=$(date '+%Y%m%d')
 hora=$(date '+%H')
 min=$(date '+%M')
 
-DIR_DESTINO="/mnt/nas/AEMET/webcams/penyiscola/$(date +'%-m')/$(date +'%d')"
+DIR_DESTINO="/mnt/nas/webcams/penyiscola/$(date +'%-m')/$(date +'%d')"
 
 wget -P "${DIR_DESTINO}" "https://s29.ipcamlive.com/streams/1drmua891bw7ontxj/snapshot.jpg"
 mv "${DIR_DESTINO}/snapshot.jpg" "${DIR_DESTINO}/penyiscola_${hora}${min}.jpg"
