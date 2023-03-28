10e3ec0cd5ee6e741345e9f0f03bf411ea98c5af
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-03-28 16:10:29 +0200

    Update scriptSatelite.sh

--- T5/scriptSatelite.sh
+++ T5/scriptSatelite.sh
@@ -3,9 +3,11 @@
 # Obtener fecha con formato "YYYYMMDD"
 DATE=$(date '+%Y%m%d')
 hora=$(date -u '+%H')
+horaesp=$(date '+%H')
 
 # Establecer directorio de destino /mnt/nas
-DIR_DESTINO="/mnt/nas/aemet/meteosat/$(date +"%Y")/$(date +"%-m")/$(date +"%d")"
+DIR_DESTINO="/home/dserna/Desktop"
 
 
-wget -P "${DIR_DESTINO}" "https://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/${DATE}${hora}00_s93g.gif"
+wget -P "${DIR_DESTINO}" "https://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/${DATE}${hora}00_s93>
+mv "${DIR_DESTINO}/${DATE}${hora}00_s93g.gif" "${DIR_DESTINO}/meteosat_${horaesp}.gif"
