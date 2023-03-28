f73165f4ea20b1e77ca08cc9660dcfb766ad282a
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-03-28 18:46:03 +0200

    Update webcamPenyagolosa.sh

--- T5/webcamPenyagolosa.sh
+++ T5/webcamPenyagolosa.sh
@@ -2,8 +2,9 @@
 
 DATE=$(date '+%Y%m%d')
 hora=$(date -u '+%H')
+min=$(date -u '+%M')
 
 DIR_DESTINO="/mnt/nas/AEMET/meteosat/$(date +"%Y")/$(date +"%-m")/$(date +"%d")"
 
 wget -P "${DIR_DESTINO}" "https://s29.ipcamlive.com/streams/1drmua891bw7ontxj/snapshot.jpg"
-mv "${DIR_DESTINO}/snapshot.jpg" "${DIR_DESTINO}/penyiscola_${hora}.jpg"
+mv "${DIR_DESTINO}/snapshot.jpg" "${DIR_DESTINO}/penyiscola_${hora}${min}.jpg"
