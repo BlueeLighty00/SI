b6849f8d37aee83232f52f3d0639a88b8a6905cc
David Serna Mateu <95046731+BlueeLighty00@users.noreply.github.com>
2023-03-28 15:31:22 +0200

    Create script.sh

---
+++ T5/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Obtener fecha con formato "YYYYMMDD"
+DATE=$(date +"%Y%m%d")
+hora=$(date -u '+%H')
+
+# Establecer directorio de destino /mnt/nas
+DIR_DESTINO="/mnt/nas/aemet/meteosat/$(date +"%Y")/$(date +"%-m")/$(date +"%d")"
+
+
+wget -P "${DIR_DESTINO}" "https://www.aemet.es/imagenes_d/eltiempo/observacion/rayos/${DATE}${hora}00_s93g.gif
